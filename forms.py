d4275dc69bdc4d5cd6115de58b712706f8f4cdbb
Nancy Dhanuka <60435345+nancydhanuka@users.noreply.github.com>
2021-08-03 15:49:25 +0530

    Add files via upload

---
+++ forms.py
@@ -0,0 +1,7 @@
+from django.forms import ModelForm
+from app.models import TODO
+
+class TODOform(ModelForm):
+    class Meta:
+        model = TODO
+        fields = ['title', 'status', 'priority']
